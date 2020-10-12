254a4017c52eba604806dad46ec64ec707432b91
Carlos Shi <carlos_shi@dlsu.edu.ph>
2020-10-12 23:08:16 +0800

    added video iframe

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,30 @@ import Helmet from "react-helmet";
 import Layout from "components/Layout";
 import Container from "components/Container";
 
+const Video = () => {
+  return (
+    <div className="video-container">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        frameBorder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+  
+};
+
 const AboutPage = () => {
   return (
     <Layout pageName="about">
       <Helmet>
-        <title>About</title>
+        <title>About ARW 2020</title>
       </Helmet>
       <Container>
+        <Video></Video>
         <h1>About</h1>
         <p>Add information about you!</p>
       </Container>
