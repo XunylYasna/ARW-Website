58b0f1304a165e3ec5f2255bd357bb5be3d284f3
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-10-25 20:02:40 +0800

    edited home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-export default function Card({ style, children, className, bgColor }) {
+export default function Card({
+  style,
+  children,
+  className,
+  bgColor,
+  dataSal,
+  duration,
+  delay,
+}) {
   return (
-
     <div
       className={`card ${className ? className : ""}`}
       style={{ backgroundColor: { bgColor }, ...style }}
+      data-sal={dataSal ? dataSal : ""}
+      data-sal-duration={duration ? duration : ""}
+      data-sal-delay={delay ? delay : ""}
     >
       {children}
     </div>
--- src/components/Home/Introduction.js
+++ src/components/Home/Introduction.js
@@ -1,20 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
 import landingVideo from "assets/images/ARW Landing.mp4";
 import Card from "components/Card";
+import { TimelineLite, Quart } from "gsap";
 
-const Introduction = () => (
-  <section className="introduction">
-    <Card className="project-head-letter introduction-content">
-      <h2>Letter from the project heads</h2>
-    </Card>
+const Introduction = () => {
+  const headerTimeline = new TimelineLite({ paused: true });
 
-    <div className="introduction-content">
-      <video width="480" height="320" autoPlay muted loop>
-        <source src={landingVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-  </section>
-);
+  useEffect(() => {
+    headerTimeline
+
+      .fromTo(".letter", 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 })
+
+      .play();
+  });
+  return (
+    <>
+      <section className="introduction">
+        <Card
+          className="project-head-letter introduction-content"
+          dataSal="slide-up"
+          duration="500"
+          delay="100"
+        >
+          <h2 className="sub-title">Letter from the project heads</h2>
+          <p>
+            ARW has and always will be an Annual Activity that is anticipated by
+            the student body. It provides an opportunity for freshmen and
+            upperclassmen alike to be able to join various organizations that
+            suit their interests. The exciting buzz around the campus filled
+            with officers advertising their respective orgs is definitely a
+            feeling to remember.
+            <br />
+            <br />
+            Although ARW2020 will be the first - and hopefully last -
+            Recruitment Week to be held online, we just want to express our
+            heartfelt gratitude to all the organizations and officers that have
+            contributed to making this event still possible even amidst these
+            difficult times. Thank you for keeping the Lasallian light of
+            student leadership burning.
+            <br />
+            <br />
+            To all the froshies, this is your chance to grab opportunities to
+            meet new people, learn new things, and discover some of the best
+            moments of your life!
+            <br />
+            <br />
+            To all the officers, thank you once again for making this possible
+            and we look forward to another year of exciting events! Keep setting
+            a good example of Lasallian leadership!
+            <br />
+            <br />
+            To everyone, let’s get ready to BUILD THIS CITY!
+            <br />
+            <br />
+            <span style={{ color: `var(--color-primary)` }}>
+              #ARW2020 #ARWBuildThisCity
+            </span>
+            <br />
+            <br />
+            The ARW 2020 Project Heads
+          </p>
+        </Card>
+      </section>
+    </>
+  );
+};
 
 export default Introduction;
