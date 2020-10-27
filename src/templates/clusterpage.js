7dea9c21eb6d0d5c632a0aea5c8a87fb437b6612
Christopher Lim <lim.christopher.go@gmail.com>
2020-10-27 20:17:26 +0800

    Add cluster icons then copy the mockup for minimap Engage

--- src/templates/clusterpage.js
+++ src/templates/clusterpage.js
@@ -11,14 +11,17 @@ export default function ClusterPage({ data }) {
     const { landingImage, title, subtitle, organizations } = data.allContentfulCluster.nodes[0]
     return (
         <Layout pageName="cluster">
-            <h1 className="organization-header">{subtitle}({title})</h1>
-            <Img fluid={landingImage.fluid} />
+            <div className="organization-header">
+                <p className="main-header">{subtitle}(<strong>{title}</strong>)</p>
+            </div>
+            <Img  draggable={false} fluid={landingImage.fluid} />
             <div className="organization-list">
-                <h1>Organization under {title}</h1>
-                <ul>
+                <h1 className="main-header" >Organizations under {title}</h1>
+                <div className="sub-line" />
+                <div className="list">
                     {organizations ? organizations.map((org, index) => {
                         return (
-                            <li key={index}>
+                            <div className="item" key={index}>
                                 <AniLink
                                     cover
                                     to={org.slug}
@@ -27,15 +30,18 @@ export default function ClusterPage({ data }) {
                                     className="header-link"
                                 >
                                     <div className="org-item">
-                                        <p>{org.organizationName}</p>
-                                        <p>{org.acronym}</p>
-                                        <Img  fluid={org.logo.fluid} />
+                                        <Img draggable={false} fluid={org.logo.fluid} />
+                                        <h1 className="sub-title">{org.organizationName}(<strong>{org.acronym}</strong>)</h1>
                                     </div>
                                 </AniLink>
-                            </li>
+                            </div>
                         )
                     }) : <div></div>}
-                </ul>
+                    <div className="item"><h1 className="sub-title">Hello</h1></div>
+                    <div className="item"><h1 className="sub-title">Hello</h1></div>
+                    <div className="item"><h1 className="sub-title">Hello</h1></div>
+                    <div className="item"><h1 className="sub-title">Hello</h1></div>
+                </div>
             </div>
         </Layout>
     )
