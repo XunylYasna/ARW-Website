573b7efa8adcb883d9a23182e500b00b3cf4c0ce
XunylYasna <lynux_ansay@dlsu.edu.ph>
2020-11-02 15:01:42 +0800

    gumana na siya swear but may ibang pages na walang image so nag if else ako

--- src/components/OrganizationTemplate/Banner.js
+++ src/components/OrganizationTemplate/Banner.js
@@ -13,6 +13,7 @@ const OrganizationBanner = ({ organizationName, acronym, videoLink, logo, backgr
     return (
         <section className="banner-container">
             <div className="organization-banner">
+                {background.fluid ? 
                 <BackgroundImage className="banner-content" fluid={background.fluid}>
                     <div>
                         <Img className="logo" fixed={logo.fixed} />
@@ -24,8 +25,24 @@ const OrganizationBanner = ({ organizationName, acronym, videoLink, logo, backgr
                             Watch the Video!
                         </button>
                     </div>
-
                 </BackgroundImage>
+                : 
+                    <div className="banner-content" style={{
+                        backgroundColor: "var(--color-primary)"
+                    }}> 
+                        <div>
+                            <Img className="logo" fixed={logo.fixed} />
+                            <h1>{organizationName}</h1>
+                            {/* <h2>{acronym}</h2> */}
+                        </div>
+                        <div className="banner-button">
+                            <button className="main-button" onClick={() => setOpen(true)}>
+                                Watch the Video!
+                            </button>
+                        </div>
+                    
+                    </div>
+                }
             </div>
 
             <ModalVideo
