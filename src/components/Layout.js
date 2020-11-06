1d207ab12e29a8c8c5b12313613e57eb2584a773
Carlos Shi <carlos_shi@dlsu.edu.ph>
2020-11-06 19:57:51 +0800

    favicon update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,9 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 import Blob from "components/Blob";
 
+import favicon96 from "../assets/favicon/favicon-96x96.png";
+import favicon192 from "../assets/favicon/android-icon-192x192.png"
+
 const Layout = ({ children, mainName, pageName }) => {
   let className = "";
 
@@ -17,7 +20,23 @@ const Layout = ({ children, mainName, pageName }) => {
 
   return (
     <>
-      <Helmet bodyAttributes={{ class: className }}>
+      <Helmet
+        bodyAttributes={{ class: className }}
+        link={[
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "96x96",
+            href: `${favicon96}`,
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "192x192",
+            href: `${favicon192}`
+          }
+        ]}
+      >
         <title>ARW 2020 || {mainName ? mainName : pageName}</title>
       </Helmet>
       <Blob numMetaballs={40} />
