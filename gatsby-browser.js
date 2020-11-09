bde13272a74230c82db76ccf68a024d37d0665dc
XunylYasna <lynux_ansay@dlsu.edu.ph>
2020-11-09 17:09:06 +0800

    gatsby-browser conflict

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,8 @@
 exports.shouldUpdateScroll = ({
-<<<<<<< HEAD
     routerProps: { location },
     getSavedScrollPosition,
 }) => {
-    window.scrollTo(0, 0)
-    return false
-}
-=======
-  routerProps: { location },
-  getSavedScrollPosition,
-}) => {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 
-  return false;
+    return false;
 };
->>>>>>> 5dd2cd5deb6849923e9f94686972f7701cd6586d
