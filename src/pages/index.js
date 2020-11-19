e71793bcdfd2b04fdc5d2809c3478ddc9baf7e06
XunylYasna <lynux_ansay@dlsu.edu.ph>
2020-11-19 19:03:12 +0800

    domain problems ba to XD

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ import Events from "components/Home/Events";
 import Partners from "components/Home/Partners";
 
 const IndexPage = () => {
-  // console.log(`%c
-  // ▒█░░░ ▒█▀▀▀█ ▒█▀▀█ ▒█▀▀▀█ 　 ▒█▀▀█ ▒█▄░▒█ ▒█▀▀▄
-  // ▒█░░░ ░▀▀▀▄▄ ▒█░░░ ░▀▀▀▄▄ 　 ▒█▄▄▀ ▒█▒█▒█ ▒█░▒█
-  // ▒█▄▄█ ▒█▄▄▄█ ▒█▄▄█ ▒█▄▄▄█ 　 ▒█░▒█ ▒█░░▀█ ▒█▄▄▀
-  // \n\t 𝒜𝓃𝓈𝒶𝓎, ℒ𝒾𝓂, 𝒮𝒽𝒾, 𝒮𝓉𝒶 ℳ𝒶𝓇𝒾𝒶 \n`, "font-family:monospace")
+  console.log(`%c
+  ▒█░░░ ▒█▀▀▀█ ▒█▀▀█ ▒█▀▀▀█ 　 ▒█▀▀█ ▒█▄░▒█ ▒█▀▀▄
+  ▒█░░░ ░▀▀▀▄▄ ▒█░░░ ░▀▀▀▄▄ 　 ▒█▄▄▀ ▒█▒█▒█ ▒█░▒█
+  ▒█▄▄█ ▒█▄▄▄█ ▒█▄▄█ ▒█▄▄▄█ 　 ▒█░▒█ ▒█░░▀█ ▒█▄▄▀
+  \n\t 𝒜𝓃𝓈𝒶𝓎, ℒ𝒾𝓂, 𝒮𝒽𝒾, 𝒮𝓉𝒶 ℳ𝒶𝓇𝒾𝒶 \n`, "font-family:monospace")
 
   return (
     <Layout mainName="Home" pageName="home">
