da108ac809438d2c682993a35e7119ff180cd8a8
XunylYasna <lynux_ansay@dlsu.edu.ph>
2020-11-21 18:16:31 +0800

    straight carousel from about section

--- src/components/OrganizationTemplate/About.js
+++ src/components/OrganizationTemplate/About.js
@@ -1,16 +1,32 @@
-import React, { useState, useEffect} from 'react'
+import React from 'react'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import Carousel from "../Carousel"
+// import Carousel from "../Carousel"
 import { FaFacebookSquare, FaTwitterSquare } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai"
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Img from 'gatsby-image'
 
 const About = ({ aboutContent, media, acronym, facebook, twitter, email }) => {
-    // console.log(media)
-    const [orgPictures, setOrgPictures] = useState([])
-
-    useEffect(() =>{
-        setOrgPictures(media)
-    }, [media])
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: items === 1 ? items : items + 2
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: items
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    }
 
     return (
         <section className="organization-about">
@@ -21,8 +37,35 @@ const About = ({ aboutContent, media, acronym, facebook, twitter, email }) => {
             </div>
 
             <div className="organization-about-carousel-container">
-                <Carousel className="carousel" media={media} items={3} style={{ textAlign: "center" }}>
+                {/* <Carousel className="carousel" media={media} items={3} style={{ textAlign: "center" }}>
                    
+                </Carousel> */}
+                <Carousel
+                    infinite={true}
+                    autoPlay={true}
+                    autoPlaySpeed={2000}
+                    keyBoardControl={true}
+                    className='carousel'
+                    responsive={responsive}
+                    renderButtonGroupOutside={true}
+                    itemClass="carousel-item-padding-40-px"
+                >
+                    {media.map((data, index) => (
+                        <div key={index} className="react-multi-carousel-item" style={{
+                            minWidth: '300px',
+                            minHeight: '300px',
+                            background: 'var(--color-primary)'
+                        }}>
+                            <Img draggable={false} fixed={data.fixed} style={{
+                                width: '100%',
+                                height: '100%',
+                                minHeight: '300px',
+                                minWidth: '300px',
+                                objectFit: 'cover',
+                            }} />
+                            {index}
+                        </div>
+                    ))}
                 </Carousel>
             </div>
 
